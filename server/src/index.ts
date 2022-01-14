2f54226c3466fef63b04b7365583e18028093f7d
Yavuz <m.yavuzyagis@gmail.com>
2022-01-14 20:54:54 +0300

    commit

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,8 +3,13 @@ import express from "express";
 const app = express();
 const port = process.env.PORT || 9000;
 
+const one:number=1;
+const two:number=2;
+
+
+
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello !");
+  res.send("Hello ! and the sum of one and two is "+(one+two));
 });
 
 app.listen(port);
