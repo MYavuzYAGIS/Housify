2fe12266016b0bf0cdc8ac109cec500a0da0b804
Yavuz <m.yavuzyagis@gmail.com>
2022-01-14 21:55:08 +0300

    commit

--- server/src/index.ts
+++ server/src/index.ts
@@ -23,7 +23,9 @@ app.post('/delete-listing', (req: Request, res: Response) => {
   for(let i=0; i<listings.length; i++){
     if(listings[i].id === id){
       return res.send(listings.splice(i,1));
+      // slice method takes where to start and how many to delete as 2 parameters. so if i is 3, it will delete the 3rd element and second parameter is 1 so deletes 1 element.
+      // splice method returns the deleted elements.
     }
- } })
+ }  })
 
 app.listen(port);
