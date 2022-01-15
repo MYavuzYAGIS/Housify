b1ea9be9858ff36043e81d6b519ec8b3073a4020
Yavuz <m.yavuzyagis@gmail.com>
2022-01-15 18:30:14 +0300

    commit

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,7 @@
 //imports
 import {ApolloServer} from 'apollo-server-express';
 import express from "express";
-
+import {schema} from "./graphql";
 
 
 //constants
@@ -9,15 +9,9 @@ const app = express();
 const port = process.env.PORT || 9000;
 //topware-middlewares
 
-const server = new ApolloServer();
-server.applyMiddleware({app, path:'/api'});
-// list listings route
-
-
-
-
-
-
-
-// app listens
-app.listen(port);
+const server = new ApolloServer({schema});
+server.start().then(()=>{
+  server.applyMiddleware({app,path:'/api'});
+  app.listen(port,()=>{
+    console.log(`Server is running on http://localhost:${port}`);
+})});
