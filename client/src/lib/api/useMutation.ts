0e441f24aec903fd71ecd062e16398674d41cf4e
Yavuz <m.yavuzyagis@gmail.com>
2022-01-28 21:04:52 +0300

    use useReducer

--- client/src/lib/api/useMutation.ts
+++ client/src/lib/api/useMutation.ts
@@ -1,4 +1,4 @@
-import { useState,useReducer } from "react";
+import { useReducer } from "react";
 import { server } from "./server";
 
 interface State<TData> {
@@ -7,50 +7,56 @@ interface State<TData> {
   error: boolean;
 }
 
+type Action<TData> =
+  | { type: "FETCH_INIT" }
+  | { type: "FETCH_SUCCESS"; payload: TData }
+  | { type: "FETCH_FAILURE" };
 
-type Action<TData> = | { type: "FETCH_INIT" } | { type: "FETCH_SUCCESS", payload: TData } | { type: "FETCH_FAILURE" }
-
-
-
-const reducer = <TData>()=>(state:State<TData>, action: Action<TData>):State<TData>=>{
+const reducer =
+  <TData>() =>
+  (state: State<TData>, action: Action<TData>): State<TData> => {
     switch (action.type) {
       case "FETCH_INIT":
-        return {...state, loading:true }
+        return { ...state, loading: true };
       case "FETCH_SUCCESS":
-        return {data:action.payload, loading:false, error:false}
+        return { data: action.payload, loading: false, error: false };
       case "FETCH_FAILURE":
-        return { ...state, loading:false, error:true}
+        return { ...state, loading: false, error: true };
       default:
         throw new Error();
-  
     }
-  }
-  
+  };
 
+type MutationTuple<TData, TVariables> = [
+  (variables?: TVariables | undefined) => Promise<void>,
+  State<TData>
+];
 
-type MutationTuple<TData, TVariables> = [(variables?: TVariables | undefined) => Promise<void>, State<TData>];
+export const useMutation = <TData = any, TVariables = any>(
+  query: string
+): MutationTuple<TData, TVariables> => {
+  const fetchReducer = reducer<TData>();
+  const [state, dispatch] = useReducer(fetchReducer, {
+    data: null,
+    loading: false,
+    error: false,
+  } as State<TData>);
 
-export const useMutation = <TData = any, TVariables = any>(query: string): MutationTuple<TData,TVariables> => {
-
-
-    const fetchReducer = reducer<TData>();
-  const [state, dispatch] = useReducer(fetchReducer, { data: null , loading: false, error:false} as State<TData>);
-
-  const fetch = async (variables?: TVariables)  => {
+  const fetch = async (variables?: TVariables) => {
     try {
-        dispatch({type:"FETCH_INIT"});
-        const { data, errors } = await server.fetch<TData, TVariables>({
+      dispatch({ type: "FETCH_INIT" });
+      const { data, errors } = await server.fetch<TData, TVariables>({
         query,
         variables,
       });
       if (errors && errors.length) {
         throw new Error(errors[0].message);
       }
-      dispatch({type:'FETCH_SUCCESS', payload:data})
+      dispatch({ type: "FETCH_SUCCESS", payload: data });
     } catch (err) {
-        dispatch({type:'FETCH_FAILURE'})
-        throw console.error(err);
+      dispatch({ type: "FETCH_FAILURE" });
+      throw console.error(err);
     }
   };
-    return [fetch, state];
+  return [fetch, state];
 };
