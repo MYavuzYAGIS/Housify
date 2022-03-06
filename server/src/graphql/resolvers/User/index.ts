f8739c080f20d551a9b4260022ff22b0213ab984
Yavuz <m.yavuzyagis@gmail.com>
2022-03-06 12:07:46 +0300

    add userInterface

--- server/src/graphql/resolvers/User/index.ts
+++ server/src/graphql/resolvers/User/index.ts
@@ -1,9 +1,22 @@
 import { IResolvers } from "@graphql-tools/utils";
+import { UserArgs } from "./types";
+import {Database,User} from "../../../lib/types";
 
 export const userResolvers:IResolvers={
     Query:{
-        user:()=>{
-            return "Query.user";
+        user: async (_root:undefined, {id}:UserArgs, {db}:{db:Database}) : Promise<User> =>{
+            try {
+                const user = await db.users.findOne({_id:id});
+                if(!user){
+                    throw new Error("User not found");
+                }
+                return user;
+                
+            } catch (error) {
+                throw new Error(`Failed to get user: ${error}`);
+            }
+                
+            }
         }
     }
-}
+
