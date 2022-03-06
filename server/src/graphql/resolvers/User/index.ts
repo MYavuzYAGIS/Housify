ed10444de6294ae5ff947422b3572438379aa2e2
Yavuz <m.yavuzyagis@gmail.com>
2022-03-06 12:34:55 +0300

    add userInterface

--- server/src/graphql/resolvers/User/index.ts
+++ server/src/graphql/resolvers/User/index.ts
@@ -1,5 +1,5 @@
 import { IResolvers } from "@graphql-tools/utils";
-import { UserArgs } from "./types";
+import { UserArgs,UserBookingArgs,UserBookingsData } from "./types";
 import { authorize } from "../../../lib/utils";
 import {Database,User} from "../../../lib/types";
 import {Request } from "express";
@@ -23,6 +23,38 @@ export const userResolvers:IResolvers={
             }
                 
             }
+        },
+        User:{
+            id: (user:User):string => user._id,
+            hasWallet: (user:User):boolean => Boolean(user.walletId),
+            income: (user:User): number | null => { return user.authorized?user.income:null},
+            bookings:async (user:User,{limit,page}:UserBookingArgs,{db}: {db:Database}) : Promise<UserBookingsData | null> =>{
+                try{
+                    if (!user.authorized){
+
+                    }
+                    const data:UserBookingsData = {
+                        total:0,
+                        result:[]
+
+                    }
+                    let cursor = await db.bookings.find({
+                        _id :{$in:user.bookings}
+                    });
+                    cursor = cursor.skip(page > 0 ? (page - 1) * limit : 0);
+                    cursor = cursor.limit(limit);
+                    data.total = await cursor.count();
+                    data.result = await cursor.toArray();
+                    return data;
+
+                }
+                catch(error){
+                    throw new Error(`Failed to query user bookings: ${error}`);
+                }
+
+
+            }
+            listings:
         }
     }
 
--- server/src/graphql/resolvers/User/types.ts
+++ server/src/graphql/resolvers/User/types.ts
@@ -1,3 +1,14 @@
+import { Booking } from "../../../lib/types";
 export interface UserArgs{
     id:string;
+}
+
+export interface UserBookingArgs{
+    limit:number;
+    page:number;
+}
+
+export interface UserBookingsData{
+    total:number;
+    result:Booking[];
 }
