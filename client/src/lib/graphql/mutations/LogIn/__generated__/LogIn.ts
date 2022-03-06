a0a73402b8d15b7901f84434b2652adbdf333244
Yavuz <m.yavuzyagis@gmail.com>
2022-03-06 13:14:59 +0300

    schema and doc regenerate

--- client/src/lib/graphql/mutations/LogIn/__generated__/LogIn.ts
+++ client/src/lib/graphql/mutations/LogIn/__generated__/LogIn.ts
@@ -3,7 +3,7 @@
 // @generated
 // This file was automatically generated and should not be edited.
 
-import { LogInInput } from "../../../globalTypes"
+import { LogInInput } from "./../src/lib/graphql/globalTypes";
 
 // ====================================================
 // GraphQL mutation operation: LogIn
@@ -23,5 +23,5 @@ export interface LogIn {
 }
 
 export interface LogInVariables {
-  input: LogInInput;
+  input?: LogInInput | null;
 }
